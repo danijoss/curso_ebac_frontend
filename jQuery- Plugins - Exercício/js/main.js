b8ad79afc04ff2648db0282bde01101665bf1fd8
Daniel <dan.henrri@gmail.com>
2023-06-26 13:29:07 -0300

    commit_incial
    commit_incial

---
+++ jQuery- Plugins - Exercício/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000-000-000-00')
+    $('#cep').mask('00000-000')
+
+
+    });
+
+    $('form').validate({
+        
+        rules: {
+            
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true,
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor insira o seu nome completo',
+            telefone: 'Por favor insira o seu telefone',
+            email: 'Por favor insira seu email',
+            cpf: 'Por favor insira o seu CPF',
+            endereco: 'Por favor insira o seu Endereço',
+            cep: 'Por favor insira seu CEP' 
+        },
+
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+        
+})
+
